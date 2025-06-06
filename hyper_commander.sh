b801e1b7f2eeab39a62028043542c2712c9bbd20
David Luke <david.luke@mailbox.org>
2025-06-06 11:42:57 +0200

    Menu function = rudem. loop
    Implemented a menu in Norton Commander style. Also, added a rudementary
    loop which calls menu function.

---
+++ hyper_commander.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+echo -e "Hello $USER"
+
+# Menu function to display option
+menu() {
+
+  echo -e "\n"
+  echo -e "------------------------------"
+  echo -e "| Hyper Commander            |"
+  echo -e "| 0: Exit                    |"
+  echo -e "| 1: OS Info                 |"
+  echo -e "| 2: User Info               |"
+  echo -e "| 3: File and Dir operations |"
+  echo -e "| 4: Find executables        |"
+  echo -e "------------------------------"
+
+  echo -ne "\n Enter your choice: "
+  read -r choice
+
+  case $choice in
+
+  0)
+    echo -e "\n Exiting..."
+    exit 0
+    ;;
+
+  1)
+    echo -e "\nNot Implemented yet."
+    ;;
+
+  2)
+    echo -e "\nNot Implemented yet."
+    ;;
+
+  3)
+    echo -e "\nNot Implemented yet."
+    ;;
+
+  4)
+    echo -e "\nNot Implemented yet."
+    ;;
+
+  *)
+    echo -e "\nInvalid choice. Please try again."
+    ;;
+  esac
+}
+
+# While loop for execution
+while true; do
+  menu
+done
