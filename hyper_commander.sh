7f83081b47cda54d84aff9bd9a6b2405821d955d
David Luke <david.luke@mailbox.org>
2025-06-10 12:02:50 +0200

    Partially implemented file and dir options
    Added a function to implement file dir option behaviour. Is not done yet
    and far from tidy.

--- hyper_commander.sh
+++ hyper_commander.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# colors
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 echo -e "Hello $USER"
 
 # Menu function to display option
@@ -36,7 +40,8 @@ menu() {
     ;;
 
   3)
-    echo -e "\nNot Implemented yet."
+    echo -e "\n"
+    file_and_dir_operations
     ;;
 
   4)
@@ -47,6 +52,44 @@ menu() {
     echo -e "\nInvalid choice. Please try again."
     ;;
   esac
+}
+
+# Function to handle file and directory operations
+file_and_dir_operations() {
+  files_and_dirs=(*)
+  echo -e "\nThe list of files and directories:"
+
+  for item in "${files_and_dirs[@]}"; do
+    if [[ -f "$item" ]]; then
+      echo "F $item"
+    elif [[ -d "$item" ]]; then
+      echo "D $item"
+    fi
+  done
+
+  echo -e "\n---------------------------------------------------"
+  echo -e "| ${RED}0${NC} Main menu | ${RED}'up'${NC} To parent | ${RED}'name'${NC} To select |"
+  echo -e "---------------------------------------------------"
+
+  read -r input
+
+  if [[ "$input" == "0" ]]; then
+    return # Go back to the main menu
+  elif [[ "$input" == [Uu]p ]]; then
+    cd .. || echo "Failed to change directory to parent."
+    file_and_dir_operations
+  elif [[ -d "$input" ]]; then
+    cd "$input" || echo "Failed to change directory to $input."
+    file_and_dir_operations
+  elif [[ -f "$input" ]]; then
+    echo -e "\nFile content of $input:"
+    cat "$input"
+    file_and_dir_operations
+  else
+    echo -e "\nInvalid input!"
+    file_and_dir_operations
+  fi
+
 }
 
 # While loop for execution
